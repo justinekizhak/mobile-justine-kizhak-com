4f0d0c1aa15ebcc4b78ad0e4e3e0bea41047aa6b
Justine Kizhakkinedath <justine@kizhak.com>
2021-04-24 21:52:52 +0530

    feat: Add all files

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Main from "@/components/Main";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Justine Kizhakkinedath</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Main />
+      <footer></footer>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    './pages/**/*.tsx',
+    './components/**/*.tsx'
+  ],
+  mode: 'jit',
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
