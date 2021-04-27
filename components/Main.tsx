710b247406c51074f901d7ce0f5dcbd13241f099
Justine Kizhakkinedath <justine@kizhak.com>
2021-04-28 01:12:53 +0530

    feat: add project section

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { css, cx } from "@emotion/css";
+import axios from "axios";
 
 const src =
   "https://res.cloudinary.com/justinekizhak/video/upload/v1612441077/bg_a37rnc.mp4";
@@ -10,6 +11,7 @@ export default function Main() {
   return (
     <main className={cx("relative", style.main)}>
       <LandingSection />
+      <ProjectSection />
       <Video />
     </main>
   );
@@ -17,7 +19,7 @@ export default function Main() {
 
 function LandingSection() {
   return (
-    <section className="font-mono uppercase">
+    <section className="justify-center px-8 font-mono uppercase">
       <div className={cx("h-[80vh]", style.sectionContent)}>
         <div>Hi there, I'm Justine.</div>
         <div>I Create</div>
@@ -27,9 +29,24 @@ function LandingSection() {
   );
 }
 
+function ProjectSection() {
+  return (
+    <section className="justify-start text-black bg-white">
+      <h1 className="p-8 text-2xl shadow-lg font-major">My Projects</h1>
+      <div className="divide-y divide-gray-300">
+        {[1, 2, 3].map((item) => (
+          <div key={item} className="p-8">
+            {item}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
 function Video() {
   return (
-    <>
+    <div>
       <div
         className={cx("bg-black opacity-50 z-[-10]", style.videoOverlay)}
       ></div>
@@ -43,8 +60,24 @@ function Video() {
         src={src}
         poster={poster}
       ></video>
-    </>
+    </div>
   );
+}
+
+const baseUrl = "https://api.storyblok.com/v1/cdn";
+const token = "QcidRr0B5ytSNhz7RCptbAtt";
+
+async function fetchProjects(pageNumber = 1, perPage = 4) {
+  const res = await axios.get(`${baseUrl}/stories`, {
+    params: {
+      version: "published",
+      token,
+      // cv: cv,
+      page: pageNumber,
+      per_page: perPage,
+    },
+  });
+  return res?.data?.stories;
 }
 
 const style = {
@@ -52,8 +85,9 @@ const style = {
     section {
       height: 100vh;
       display: flex;
-      align-items: center;
-      padding: 0 2rem;
+      flex-direction: column;
+      /* justify-content: center; */
+      /* padding: 0 2rem; */
     }
   `,
   sectionContent: css`
